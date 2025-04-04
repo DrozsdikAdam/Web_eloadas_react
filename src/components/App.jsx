2f63acab914471f3f6d9752d903c497d5d26f5ba
DrozsdikAdam <drozsdik17@gmail.com>
2025-04-04 22:13:56 +0200

    főoldal kezdete

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppStyles from '../styles/app.module.css'
+import Placeholder from '../images/image.png'
+function App() {
+
+  return (
+    <>
+      <div className={AppStyles.container}>
+        <div className={AppStyles.card}>
+          <h1 className={AppStyles.title}>Tic-tac-toe Game</h1>
+          
+        </div>
+        <div className={AppStyles.card}>a</div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderStyles from '../styles/header.module.css'
+
+const Header = () => {
+    return(
+        <>
+            <header className={HeaderStyles.header}>
+                <h1>Webprogramozás React Project feladat</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
