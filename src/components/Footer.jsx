8dbb2615dd684ff23504e86a1882e32dec08ba74
DrozsdikAdam <drozsdik17@gmail.com>
2025-04-08 12:23:30 +0200

    footer elkészítve

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FooterStyles from '../styles/footer.module.css';
+
+const Footer = () => {
+return(
+    <footer className={FooterStyles.footer}>
+        <p>Drozsdik Ádám - VRB9YV</p>
+        <p>Gacov Richárd Rómeó - QDT8Y7</p>
+    </footer>
+);
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
 import React from 'react'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 import './main.css'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Header />
       <App />
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 )
